08c8a2a0495dc52b5600b2350b9bc0d10e57a848
Никита <nbarannik165@gmail.com>
2023-08-10 21:44:23 +0300

    Create routes

--- pkg/routes/gamestore-routes.go
+++ pkg/routes/gamestore-routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/nbarannik/gameshop-go-crud-api/pkg/controllers"
+)
+
+func RegisterGameStoreRoutes(router *mux.Router) {
+	router.HandleFunc("/game/", controllers.GetGame).Methods("GET")
+	router.HandleFunc("/game/", controllers.CreateGame).Methods("POST")
+	router.HandleFunc("/game/{gameId}", controllers.GetGameById).Methods("GET")
+	router.HandleFunc("/game/{gameId}", controllers.UpdateGame).Methods("PUT")
+	router.HandleFunc("/game/{gameId}", controllers.DeleteGame).Methods("DELETE")
+}
