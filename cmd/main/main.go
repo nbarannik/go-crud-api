35e930146f408809e0e3be3da94e8ee3f3df86af
Никита <nbarannik165@gmail.com>
2023-08-13 22:28:13 +0300

    Add server start

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/nbarannik/gameshop-go-crud-api/pkg/routes"
+	"log"
+	"net/http"
+)
+
+func main() {
+	router := mux.NewRouter()
+	routes.RegisterGameStoreRoutes(router)
+	http.Handle("/", router)
+	log.Fatal(http.ListenAndServe("localhost:5000", router))
+}
