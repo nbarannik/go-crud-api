b45275d8d770ede7e187d6cab9c5af4cb2680a00
Никита <nbarannik165@gmail.com>
2023-08-13 21:18:07 +0300

    add ParseBody function in utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,1 +1,18 @@
 package utils
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func ParseBody(r *http.Request, i interface{}) {
+	requestBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(requestBody, i)
+	if err != nil {
+		panic(err)
+	}
+}
