62114995a09220dd7fb91b5d1c42d1d07b89d56c
Никита <nbarannik165@gmail.com>
2023-08-14 16:19:14 +0300

    Add db functions

--- pkg/models/game.go
+++ pkg/models/game.go
@@ -1,1 +1,47 @@
 package models
+
+import (
+	"github.com/nbarannik/gameshop-go-crud-api/pkg/config"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+type Game struct {
+	gorm.Model
+	Title  string `gorm:""json:"title"`
+	Author string `json:"author"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	err := db.AutoMigrate(&Game{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (g *Game) CreateGame() *Game {
+	//TODO: Add check if the same value is already in the table
+	db.Create(&g)
+	return g
+}
+
+func GetGames() []Game {
+	var games []Game
+	db.Find(&games)
+	return games
+}
+
+func GetGameById(Id int64) (*Game, *gorm.DB) {
+	var game Game
+	db := db.Where("ID=?", Id).Find(&game)
+	return &game, db
+}
+
+func DeleteGame(Id int64) Game {
+	var game Game
+	db.Where("ID=?", Id).Delete(game)
+	return game
+}
