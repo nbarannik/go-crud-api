9979ad597c57a6c2a6147bf205831969971a4f23
Никита <nbarannik165@gmail.com>
2023-08-14 23:19:17 +0300

    Add correct user and password info

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/nbarannik/gameshop-go-crud-api/pkg/routes"
 	"log"
@@ -11,5 +12,6 @@ func main() {
 	router := mux.NewRouter()
 	routes.RegisterGameStoreRoutes(router)
 	http.Handle("/", router)
+	fmt.Println("Run server on localhost:5000")
 	log.Fatal(http.ListenAndServe("localhost:5000", router))
 }
--- pkg/config/database.go
+++ pkg/config/database.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -8,12 +9,13 @@ import (
 var connector *gorm.DB
 
 func Connect() {
-	dsn := "user:pass@/dbname?charset=utf8&parseTime=True&loc=Local" //TODO: add func to make custom dsn
+	dsn := "root:password@/gameshop?charset=utf8&parseTime=True&loc=Local" //TODO: add func to make custom dsn
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 	connector = db
+	fmt.Println("connected")
 }
 
 func GetDB() *gorm.DB {
